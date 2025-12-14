c7ba547c3f9681515bb6f57e585351044eb8e0dc
molten-metal2 <jackbunckenburg@gmail.com>
2025-12-14 16:07:11 +1300

    feat: update template to chicken-website

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket for website hosting (must be globally unique)"
   type        = string
-  default     = "my-website-template-bucket"  # CUSTOMIZE: Change to your unique bucket name
+  default     = "chicken-website-bucket
 }
 
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
-  default     = "ap-southeast-2"  # Change to your preferred region
+  default     = "ap-southeast-2"
 }
 
